68969f475e986452c6a4a90eebc064327392fab6
Jimmy Hsu <jimmyhsu@live.com>
2015-04-27 13:27:55 -0700

    Add basic test and routes Testing with Mocha and Chai

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // dependencies
 var express   = require('express');
 
+// routes
+var json2csv = require('.lib/routes/json2csv');
+var csv2json = require('.lib/routes/csv2json');
+
 // setup
 var app = express();
 
 // set port
 app.set('port', process.env.PORT || 1337);
+
+// set up api with versioning
+app.use('/api/v1', json2csv);
+app.use('/api/v1', csv2json);
 
 var server = app.listen(app.get('port'), function() {
   console.log('json2csv listening on port ' + server.address().port);
---
+++ test/test.js
@@ -0,0 +1,19 @@
+var expect = require("chai").expect;
+var json2csv = require("../lib/routes/json2csv.js");
+var csv2json = require("../lib/routes/csv2json.js");
+
+describe("Converting JSON to CSV", function() {
+   describe("/json2csv", function() {
+       it("should convert JSON to CSV", function(){
+
+       });
+   });
+});
+
+describe("Converting CSV to JSON", function() {
+   describe("/csv2json", function() {
+       it("should convert JSON to CSV", function(){
+
+       });
+   });
+});
