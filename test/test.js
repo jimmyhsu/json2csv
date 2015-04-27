980c3c574ef4221d2b8945af331b540ca4492abb
Jimmy Hsu <jimmyhsu@live.com>
2015-04-27 13:58:23 -0700

    Add testing for bad requests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
-var expect = require("chai").expect;
+// var expect = require("chai").expect;
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+var app = require("../app.js");
 var json2csv = require("../lib/routes/json2csv.js");
 var csv2json = require("../lib/routes/csv2json.js");
 
+chai.use(chaiHttp);
+
 describe("Converting JSON to CSV", function() {
   describe("/json2csv", function() {
+    it("should give a 400 code for empty POST requests", function() {
+      chai.request(app)
+        .post('/user/me')
+        .send({})
+        .end(function (err, res) {
+           expect(err).to.have.status(400);
+        });
+    });
     it("should convert JSON to CSV", function() {
 
     });
