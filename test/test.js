388e7b111e79f64c188c89fd3094478c0ba5062d
Jimmy Hsu <jimmyhsu@live.com>
2015-04-27 13:29:56 -0700

    Fix test whitespacing

--- test/test.js
+++ test/test.js
@@ -3,17 +3,17 @@ var json2csv = require("../lib/routes/json2csv.js");
 var csv2json = require("../lib/routes/csv2json.js");
 
 describe("Converting JSON to CSV", function() {
-   describe("/json2csv", function() {
-       it("should convert JSON to CSV", function(){
+  describe("/json2csv", function() {
+    it("should convert JSON to CSV", function() {
 
-       });
-   });
+    });
+  });
 });
 
 describe("Converting CSV to JSON", function() {
-   describe("/csv2json", function() {
-       it("should convert JSON to CSV", function(){
+  describe("/csv2json", function() {
+    it("should convert JSON to CSV", function() {
 
-       });
-   });
+    });
+  });
 });
