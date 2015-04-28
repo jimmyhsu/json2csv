20573833ca942a9bb62377affce9d0e0c3f1ce0d
Jimmy Hsu <jimmyhsu@live.com>
2015-04-28 00:41:08 -0700

    Add test for csv2json

--- test/test.js
+++ test/test.js
@@ -58,15 +58,64 @@ describe("Converting JSON to CSV", function() {
 });
 
 describe("Converting CSV to JSON", function() {
+
   describe("/csv2json", function() {
-    it("should convert CSV to JSON", function() {
+
+    it("should give a 400 code for empty POST requests", function(done) {
       chai.request(app)
-        .post('/')
-        .send()
+        .post('/api/v1/csv2json')
+        .send({})
         .end(function(err, res) {
-          expect(res).to.have.status(200);
+          expect(res).to.have.status(400);
           done();
         });
     });
+
+    it("should have a content type of appliation/json & charset of utf-8", function(done) {
+      chai.request(app)
+        .post('/api/v1/csv2json')
+        .send(['name,lastName,gender,country\n' +
+               'Adam,Smith,male,Scotland\n' +
+               'George,Washington,male,USA\n' +
+               'Marie,Curie,female,France\n'])
+        .end(function(err, res) {
+          expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+          done();
+        });
+    });
+
+    it("should convert CSV to JSON", function(done) {
+      chai.request(app)
+        .post('/api/v1/csv2json')
+        .send(['name,lastName,gender,country\n' +
+                    'Adam,Smith,male,Scotland\n' +
+                    'George,Washington,male,USA\n' +
+                    'Marie,Curie,female,France\n'])
+        .end(function(err, res) {
+          // console.log(res);
+          expect(res).to.have.status(200);
+          var json = [{
+            "name": "Adam",
+            "lastName": "Smith",
+            "gender": "male",
+            "country": "Scotland"
+          }, {
+            "name": "George",
+            "lastName": "Washington",
+            "gender": "male",
+            "country": "USA"
+          }, {
+            "name": "Marie",
+            "lastName": "Curie",
+            "gender": "female",
+            "country": "France"
+          }];
+          // stringify for testing purposes
+          expect(JSON.stringify(res.body)).to.equal(JSON.stringify(json));
+          done();
+        });
+    });
+
   });
+
 });
